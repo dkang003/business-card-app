6f68f1f1d5fff510523ab1821b545ea0f1d41bad
Daniel Kang <d.kang106@gmail.com>
2019-01-12 23:14:13 -0800

    finished auth part 1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :authorize, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_session_path
+    # else
+    #   something
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  resources :users do
+    resources :cards
+  end
+  resources :sessions, only: [:new, :create]
+  delete '/logout' => 'sessions#destroy', as: :logout
 end
