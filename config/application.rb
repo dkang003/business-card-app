fd642073fd57b6889015a4f6d183d2726504584e
Daniel Kang <d.kang106@gmail.com>
2019-01-20 21:05:57 -0800

    config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -17,13 +17,14 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
+
 
 module BusCardApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
